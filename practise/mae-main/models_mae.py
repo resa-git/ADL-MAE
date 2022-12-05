029b4d34c0ffe2bfdb150d33b4748f7215ea0b02
resa-git <42516074+resa-git@users.noreply.github.com>
2022-12-05 08:12:28 +0100

    chnage deconder depth to 1 in official pytorch

--- practise/mae-main/models_mae.py
+++ practise/mae-main/models_mae.py
@@ -223,7 +223,7 @@ class MaskedAutoencoderViT(nn.Module):
 def mae_vit_base_patch16_dec512d8b(**kwargs):
     model = MaskedAutoencoderViT(
         patch_size=16, embed_dim=768, depth=12, num_heads=12,
-        decoder_embed_dim=512, decoder_depth=8, decoder_num_heads=16,
+        decoder_embed_dim=512, decoder_depth=1, decoder_num_heads=16,
         mlp_ratio=4, norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
     return model
 
